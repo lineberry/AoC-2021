6b92387212cee2b597f19fca7799e531a2e183bc
Chad Lineberry <chad@tcgplayer.com>
2021-12-01 10:22:09 -0500

    Solution for day 1

--- Day-1/program.go
+++ Day-1/program.go
@@ -3,21 +3,25 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+	part2()
+}
+
+func readLines(path string) ([]int, error) {
+	file, err := os.Open(path)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer file.Close()
 
+	var lines []int
 	scanner := bufio.NewScanner(file)
-	// optionally, resize scanner's capacity for lines over 64K, see next example
 	for scanner.Scan() {
+		// Convert to int
 		i, err := strconv.Atoi(scanner.Text())
 
 		if err != nil {
@@ -26,10 +30,69 @@ func main() {
 			os.Exit(2)
 		}
 
-		fmt.Println(i)
+		lines = append(lines, i)
+	}
+	return lines, scanner.Err()
+}
+
+func part1() {
+	inputSlice, err := readLines("input.txt")
+
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+		os.Exit(2)
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	prev, increaseCount := 0, 0
+
+	for _, value := range inputSlice {
+		if prev == 0 {
+			prev = value
+			continue
+		}
+		if value > prev {
+			increaseCount++
+		}
+		prev = value
+
 	}
+
+	fmt.Println(increaseCount)
+}
+
+func part2() {
+	inputSlice, err := readLines("input.txt")
+
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+		os.Exit(2)
+	}
+
+	prev, increaseCount := 0, 0
+
+	for index := range inputSlice {
+
+		slidingWindow := inputSlice[index : index+3]
+		slidingSum := 0
+
+		if slidingWindow[len(slidingWindow)-1] == 0 {
+			continue
+		}
+
+		fmt.Println(slidingWindow)
+
+		for _, slidingValue := range slidingWindow {
+			slidingSum += slidingValue
+		}
+
+		if index > 0 && slidingSum > prev {
+			increaseCount++
+		}
+		prev = slidingSum
+
+	}
+
+	fmt.Println(increaseCount)
 }
