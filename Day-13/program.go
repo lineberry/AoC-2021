f84dab97e8bfc7f76eab1584fce4bd128ee8851b
Chad Lineberry <chad@tcgplayer.com>
2021-12-15 23:23:06 -0500

    Day 13 WIP

--- Day-13/program.go
+++ Day-13/program.go
@@ -9,156 +9,99 @@ import (
 	"github.com/spf13/cast"
 )
 
-type MapGrid struct {
-	Grid [][]int
+type Coordinate struct {
+	x, y int
 }
 
-type Line struct {
-	x1, y1 int
-	x2, y2 int
+type FoldInstruction struct {
+	foldIndex int
+	foldAxis  string
 }
 
 func main() {
-	mapGrid := MakeMapGrid()
-	lines, err := readLines("input.txt")
+	grid, coords, instructions, err := readLines("input-test.txt")
 
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(2)
 	}
 
-	Part1(lines, &mapGrid)
+	Part1(&grid, coords, instructions)
 }
 
-func readLines(path string) ([]Line, error) {
+func readLines(path string) ([][]string, []Coordinate, []FoldInstruction, error) {
+	gridHeight := 15
+	gridWidth := 11
+	readInstructions := false
+
+	grid := make([][]string, gridHeight)
+	var foldInstructions []FoldInstruction
+	var coords []Coordinate
+	for y := 0; y < len(grid); y++ {
+		grid[y] = make([]string, gridWidth)
+		for x := 0; x < gridWidth; x++ {
+			grid[y][x] = " "
+		}
+	}
+
 	file, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return nil, nil, nil, err
 	}
 	defer file.Close()
-
-	lines := make([]Line, 500)
 	scanner := bufio.NewScanner(file)
-	currentLineIndex := 0
 
 	for scanner.Scan() {
-		xAndYs := strings.Split(scanner.Text(), " -> ")
-		x1y1 := strings.Split(xAndYs[0], ",")
-		x2y2 := strings.Split(xAndYs[1], ",")
+		scannerText := scanner.Text()
+		if len(scannerText) == 0 {
+			readInstructions = true
+			continue
+		}
 
-		lines[currentLineIndex] = Line{x1: cast.ToInt(x1y1[0]), y1: cast.ToInt(x1y1[1]), x2: cast.ToInt(x2y2[0]), y2: cast.ToInt(x2y2[1])}
-		currentLineIndex++
-	}
-	return lines, scanner.Err()
-}
-
-func MakeMapGrid() MapGrid {
-	var rv MapGrid
-	emptyGrid := make([][]int, 1000)
-	for i := 0; i < len(emptyGrid); i++ {
-		emptyGrid[i] = make([]int, 1000)
-	}
-	rv.Grid = emptyGrid
-	return rv
-}
-
-func DrawLine(line Line, mapGrid *MapGrid) {
-	slope := GetSlope(line)
-
-	//fmt.Println("Line ", line, " has a slope of ", slope)
-
-	switch slope {
-	case "-0":
-		if line.y2 > line.y1 {
-			for i := 0; i <= line.y2-line.y1; i++ {
-				mapGrid.Grid[line.y1+i][line.x1] += 1
-			}
+		if !readInstructions {
+			xAndY := strings.Split(scannerText, ",")
+			x := cast.ToInt(xAndY[0])
+			y := cast.ToInt(xAndY[1])
+			coords = append(coords, Coordinate{x: x, y: y})
+			grid[y][x] = "█"
 		} else {
-			for i := 0; i <= line.y1-line.y2; i++ {
-				mapGrid.Grid[line.y2+i][line.x1] += 1
-			}
-		}
-
-	case "0":
-		if line.x2 > line.x1 {
-			for i := 0; i <= line.x2-line.x1; i++ {
-				mapGrid.Grid[line.y1][line.x1+i] += 1
-			}
-		} else {
-			for i := 0; i <= line.x1-line.x2; i++ {
-				mapGrid.Grid[line.y1][line.x2+i] += 1
-			}
-		}
-
-	case "1":
-		if line.x2 > line.x1 {
-			for i := 0; i <= line.x2-line.x1; i++ {
-				mapGrid.Grid[line.y1+i][line.x1+i] += 1
-			}
-		} else {
-			for i := 0; i <= line.x1-line.x2; i++ {
-				mapGrid.Grid[line.y2+i][line.x2+i] += 1
-			}
-		}
-	case "-1":
-		if line.x2 > line.x1 {
-			for i := 0; i <= line.x2-line.x1; i++ {
-				mapGrid.Grid[line.y1-i][line.x1+i] += 1
-			}
-		} else {
-			for i := 0; i <= line.x1-line.x2; i++ {
-				mapGrid.Grid[line.y2-i][line.x2+i] += 1
-			}
+			foldFields := strings.Fields(scannerText)
+			axisAndIndex := strings.Split(foldFields[2], "=")
+			foldAxis := axisAndIndex[0]
+			foldIndex := cast.ToInt(axisAndIndex[1])
+			foldInstructions = append(foldInstructions, FoldInstruction{foldAxis: foldAxis, foldIndex: foldIndex})
 		}
 	}
 
-	//PrintMapGrid(*mapGrid)
+	return grid, coords, foldInstructions, scanner.Err()
 }
 
-func Abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func GetSlope(line Line) string {
-	if line.x2-line.x1 == 0 { //vertical
-		return "-0"
-	} else if line.y2-line.y1 == 0 { //horizontal
-		return "0"
-	} else if (line.y2-line.y1)/(line.x2-line.x1) > 0 {
-		return "1"
-	} else {
-		return "-1"
+func PrintGrid(grid *[][]string) {
+	for y := 0; y < len(*grid); y++ {
+		fmt.Println((*grid)[y])
 	}
 }
 
-func GetCountOfOverlaps(numToLookFor int, mapGrid *MapGrid) int {
-	var rv int
-	for i := 0; i < len(mapGrid.Grid); i++ {
-		for j := 0; j < len(mapGrid.Grid[i]); j++ {
-			if mapGrid.Grid[i][j] >= numToLookFor {
-				rv++
-			}
-		}
+func GetFoldedCoordinate(coord Coordinate, instruction FoldInstruction) Coordinate {
+	yNew := instruction.foldIndex - (coord.y - instruction.foldIndex)
+	xNew := instruction.foldIndex - (coord.x - instruction.foldIndex)
+	rv := Coordinate{x: xNew, y: yNew}
+	switch instruction.foldAxis {
+	case "y":
+		rv.x = coord.x
+	case "x":
+		rv.y = coord.y
 	}
 	return rv
 }
 
-func PrintMapGrid(mapGrid MapGrid) {
-	for i := 0; i < len(mapGrid.Grid); i++ {
-		fmt.Println(mapGrid.Grid[i])
-	}
+func Fold(grid *[][]string, coords []Coordinate, instruction FoldInstruction) {
+	fmt.Println("Fold")
 }
 
-func Part1(lines []Line, mapGrid *MapGrid) {
-	for _, line := range lines {
-		DrawLine(line, mapGrid)
+func Part1(grid *[][]string, coords []Coordinate, instructions []FoldInstruction) {
+	PrintGrid(grid)
+	for _, i := range instructions {
+		Fold(grid, coords, i)
 	}
-
-	//PrintMapGrid(*mapGrid)
-
-	part1Answer := GetCountOfOverlaps(2, mapGrid)
-	fmt.Println(part1Answer)
 }
